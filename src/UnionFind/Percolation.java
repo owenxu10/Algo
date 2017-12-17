e36c96a658b4d3bf1be7a084174d5c6966b0c3aa
Owen Xu <owenxu10@aliyun.com>
2017-12-16 19:59:18 -0500

    [Refractor] Add the percolation test code

---
+++ src/UnionFind/Percolation.java
@@ -0,0 +1,31 @@
+package UnionFind;
+
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdDraw;
+
+import java.awt.*;
+
+public class Percolation {
+
+
+    public Percolation(int number){
+
+    }
+
+    public void open(int row, int col){
+
+    }
+
+    public boolean isOpen(int row, int col){
+        return true;
+    }
+
+    public boolean isFull(int row, int col){
+        return true;
+    }
+
+    public boolean percolates(){
+        return true;
+    }
+
+}
